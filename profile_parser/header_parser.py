6fcbfda2d0c672a95fe06648890da11d62875b13
Shaxzod <shaxzodbekbozorov@gmail.com>
2022-10-08 16:27:50 +0500

    check for credentials

--- profile_parser/header_parser.py
+++ profile_parser/header_parser.py
@@ -1,11 +1,8 @@
-import json
 import os
 import pickle
-from datetime import timedelta, datetime
+from datetime import datetime
 from pprint import pprint
 
-import redis
-import requests
 from bs4 import BeautifulSoup
 from environs import Env
 from lxml import etree
@@ -23,21 +20,24 @@ env = Env()
 env.read_env()
 
 
+class InvalidCredentials(Exception):
+    def __init__(self, message="Provide a valid not null credentials"):
+        self.message = message
+        super().__init__(self.message)
+
+
 class InstagramAuth:
-    """I could've use Union for the driver argument but,
+    """I could've use Union for the driver argument in constructor but,
      I ended up choosing chrome driver to make the type hints work  """
 
-    def __init__(self, username: str, password: str, driver: webdriver.Chrome):
-        self.username = username
-        self.password = password
+    def __init__(self, driver: webdriver.Chrome):
         self._driver = driver
         self._driver.maximize_window()
-        self.redis_cache = redis.Redis()
 
     def close_browser(self):
         self._driver.close()
 
-    def check_and_update_cookies(self):
+    def check_and_update_cookies(self) -> bool:
         """
         Selenium webdriver is initialized with default url "data:".
         add_cookie() requires the current url to be under the same domain pattern as the cookie otherwise
@@ -46,7 +46,7 @@ class InstagramAuth:
         :return:
         """
         if not os.path.exists("cookies.pkl"):
-            return None
+            return False
         with open("cookies.pkl", "rb") as f:
             cookies = pickle.load(f)
             # there might be dicts without "expiry" key, so there's an additional check
@@ -60,18 +60,19 @@ class InstagramAuth:
                 self._driver.add_cookie(cookie_dict=cookie)
         return True
 
-    def login(self):
-        cookies_exists = self.check_and_update_cookies()
-        if cookies_exists:
+    def login(self, username: str, password: str) -> None:
+        if not username or not password:
+            raise InvalidCredentials
+        if self.check_and_update_cookies():
             return
         self._driver.get('https://www.instagram.com/accounts/login/')
         WebDriverWait(self._driver, 10).until(ec.presence_of_element_located((By.XPATH, "//input[@name='username']")))
         user_name_elem = self._driver.find_element(By.XPATH, "//input[@name='username']")
-        user_name_elem.clear()
-        user_name_elem.send_keys(self.username)
         password_elem = self._driver.find_element(By.XPATH, "//input[@name='password']")
+        user_name_elem.clear()
         password_elem.clear()
-        password_elem.send_keys(self.password)
+        user_name_elem.send_keys(username)
+        password_elem.send_keys(password)
         password_elem.send_keys(Keys.RETURN)
         try:
             WebDriverWait(self._driver, 10).until(
@@ -83,7 +84,7 @@ class InstagramAuth:
         pickle.dump(self._driver.get_cookies(), open("cookies.pkl", "wb"))
 
     @property
-    def get_driver(self):
+    def get_driver(self) -> webdriver.Chrome:
         return self._driver
 
 
@@ -123,7 +124,7 @@ class InstagramBot:
         img_src_list = list(dict.fromkeys(img_src_list))
         return img_src_list
 
-    def get_profile_page(self, profile_page_url: str):
+    def get_profile_page(self, profile_page_url: str) -> str:
         self._driver.get(profile_page_url)
         try:
             WebDriverWait(self._driver, 10).until(ec.all_of(
@@ -147,57 +148,42 @@ class BaseParser:
 class HeaderParse(BaseParser):
 
     def parse_avatar_url(self) -> str:
-        user_avatar = self.dom.xpath("//header/div/div/span/img/@src")
-        user_avatar_1 = self.dom.xpath("//header/div/div/div/button/img/@src")
-        return user_avatar[0] or user_avatar_1[0]
+        user_avatar = self.dom.xpath("//header//img/@src")
+        if not user_avatar:
+            return ""
+        return user_avatar.pop()
 
-    def get_basic_info(self):
+    def parse_username(self) -> str:
+        user_name_1 = self.dom.xpath("//header//*[self::h1 or self::h2]/text()")
+        user_name_2 = self.soup.find(class_="_aacl _aacs _aact _aacx _aada")
+        username = user_name_1.pop() if user_name_1 else user_name_2.text
+        return username
+
+    def parse_followers_posts(self, text: str) -> str:
+        element = self.dom.xpath(f"//div[contains(text(), \'{text}\')]/span/text()")
+        if not element:
+            return ""
+        return element.pop()
+
+    def get_basic_info(self) -> str:
         user_avatar_url = self.parse_avatar_url()
-        user_name = self.soup.find(class_="_aacl _aacs _aact _aacx _aada")
-        user_info = self.soup.findAll("span", "_ac2a")
-        user_posts_count = user_info[0]
-        user_followers_count = user_info[1]
-        user_following_count = user_info[2]
-        return f"User Name: {user_name.text}\nAvatar Url: {user_avatar_url}\nPosts: {user_posts_count.text}\n" \
-               f"Followers: {user_followers_count.text}\nFollowing: {user_following_count.text}"
-
-
-class PostsParse(BaseParser):
-
-    def _get_post_img_src(self, class_name: str) -> list:
-        img_src_list = list()
-        child_divs = self.soup.find("div", class_=class_name)
-        for img in child_divs.findAll("img"):
-            img_src_list.append(img["srcset"].split(",")[-1].split()[0])
-        return img_src_list
-
-    def get_user_posts(self):
-        post_row_block = self.dom.xpath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div["
-                                        "2]/section/main/div/div[2]/article/div[1]/div/div[1]")
-        if post_row_block:
-            post_row_block = post_row_block[0]
-        else:
-            raise NoSuchElementException
-        block_class_name = post_row_block.attrib["class"]
-        src_list = self._get_post_img_src(block_class_name)
-        return src_list
+        user_name = self.parse_username()
+        user_posts_count = self.parse_followers_posts("posts")
+        user_followers_count = self.parse_followers_posts("followers")
+        user_following_count = self.parse_followers_posts("following")
+        return f"User Name: {user_name}\nAvatar Url: {user_avatar_url}\nPosts: {user_posts_count}\n" \
+               f"Followers: {user_followers_count}\nFollowing: {user_following_count}"
 
 
 if __name__ == "__main__":
-    UserIG = InstagramAuth(env.str("IG_USERNAME"), env.str("IG_PASSWORD"),
-                           webdriver.Chrome())  # Add Account and Password
-    UserIG.login()
+    UserIG = InstagramAuth(webdriver.Chrome())
+    UserIG.login(env.str("IG_USERNAME"), env.str("IG_PASSWORD"))
     Insta_bot = InstagramBot(UserIG.get_driver)
-    user_page = Insta_bot.get_profile_page("https://www.instagram.com/balabanoova/")
+    user_page = Insta_bot.get_profile_page("https://www.instagram.com/shvhzxd/")
     user_header = HeaderParse(user_page)
-    src_list = Insta_bot.parse_posts_links()
+    # src_list = Insta_bot.parse_posts_links()
     print(user_header.get_basic_info())
-    print(src_list)
-    print(len(src_list))
+    # print(src_list)
+    # print(len(src_list))
     UserIG.close_browser()
-    # with open("index.html", "r", encoding="utf-8") as fp:
-    #     user_page = fp.read()
-    # user_header = HeaderParse(user_page)
-    # user_posts = PostsParse(user_page)
-    # print(user_header.get_basic_info())
-    # print(user_posts.get_user_posts())
+
